b4b775d888290f52f4f82cd3199efb9dce8fbea4
eliottfnt <eliott.ferrant@free.fr>
2025-10-24 21:36:00 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{fontFamily: 'sans-serif', padding: '2rem'}}>
+      <h1>Mom & Pet Boutique</h1>
+      <p>Bienvenue dans la boutique pour mamans, bébés et animaux !</p>
+      <a href="/products"><button>Découvrir les produits</button></a>
+    </div>
+  );
+}
